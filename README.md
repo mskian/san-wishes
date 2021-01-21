893fd0760991e0fec36fcb64578f9a74075e00c1
mskian <veerfun23@gmail.com>
2021-01-21 12:34:10 +0530

    ✨ New Features and 🔼 Update

--- README.md
+++ README.md
@@ -4,8 +4,49 @@
 
 San Wishes - A Simple Wishing site Built using React.
 
-⚡ Speed | ☁ Offline | 🌩 PWA
+⚛ React | ⚡ Speed | ☁ Offline | 🌩 PWA
 
-## LICENSE
+This is My Personal Wishing Site - Share the Festival Wishes via Web link
+
+## Features 🍔
+
+- React JS
+- PWA
+- Offline Suppport
+- Bulma CSS
+- Power Packed with React Modules
+
+## Installation 📦
+
+- Clone this repo or Download
+
+```sh
+git clone https://github.com/mskian/san-wishes.git
+cd san-wishes
+yarn install
+```
+
+- Test the site
+
+```sh
+yarn start
+```
+
+- Build the site
+
+```sh
+yarn build
+```
+
+- Test the production Build on Localhost
+
+```sh
+npm install -g serve
+yarn server
+```
+
+- Production Build avilable in the folder `build`
+
+## LICENSE ☑
 
 MIT
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.min.css'
 import './index.css';
-import { Fade, Bounce, Slide } from "react-awesome-reveal";
-import ReactCountryFlag from "react-country-flag"
+import { Fade, Bounce, Slide} from "react-awesome-reveal";
+import ReactCountryFlag from "react-country-flag";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Typist from 'react-typist';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 class RetrieveList extends Component {
+
+  RefreshPage = () => {
+    this.PageToast();
+ }
+ Pagewish() {
+  window.location.reload();
+}
+ PageToast() {
+  toast.info('🦄 Welcome', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+    });
+  }
  
   render() {
     return (
@@ -14,7 +37,7 @@ class RetrieveList extends Component {
         <div className="column is-half">
         <Slide triggerOnce>
         <h1 className="title has-text-warning has-text-centered">
-         Happy Republic Day
+        <Typist avgTypingDelay={40}  startDelay={1800} cursor={{ hideWhenDone: true }}> Happy Republic Day</Typist>
         </h1>
         </Slide>
         <br />
@@ -27,10 +50,43 @@ class RetrieveList extends Component {
                     width: '100%',
                     height: '100%',
                 }}
-                title="US"
+                title="India"
             />
         </Bounce>
         </Fade>
+        <br />
+        <br />
+        <Slide triggerOnce>
+        <div className="notification has-text-weight-bold read-more">
+        <p className="has-text-weight-bold has-text-left">
+         We got our independence by losing many brave souls<br /><br />
+         We got India fully republic by many efforts<br /><br />
+         On this republic day let's salute to all of them<br /><br/>
+         Who served for the country<br /><br />
+         And never forget their sacrifices<br /><br />
+         Happy Republic Day 2021 🇮🇳
+        </p>
+        </div>
+        </Slide>
+        <br />
+        <div class="buttons is-centered">
+        <ToastContainer />
+        <button className="button is-link read-random"  onClick={() => window.location.reload()}>Get My Wish</button>
+        <button className="button is-warning read-random" onClick={this.RefreshPage}>Thank you</button>
+        </div>
+        <br />
+        <AudioPlayer autoPlay src="https://san-movies.b-cdn.net/videos/india-sg2.mp3" showSkipControls={true} showJumpControls={false} />
+        <br />
+        <br />
+        <hr />
+        <p class="has-text-success has-text-weight-bold has-text-centered">
+          My Personal Wishing web page for Sharing My Greetings and Wishes Around the World - Built using React JS <br />
+          <br />
+          <Fade>
+          <img src="./react.png" loading="lazy" height="45" width="45" alt="San Wishes" />
+          </Fade>
+        </p>
+        <br />
         <br />
         </div>
         </div>
